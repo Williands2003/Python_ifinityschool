e975cda710d8dc14c305df4c6f7456bfa25da4f0
WILLIAN DOUGLAS <67987291+Williands2003@users.noreply.github.com>
2024-10-30 21:58:46 -0300

    Create atividade2.py

---
+++ atividade2.py
@@ -0,0 +1,8 @@
+contato = {}
+
+# Solicitando que o usuário insira os dados do contato
+contato['nome'] = input("Digite o nome do contato: ")
+contato['telefone'] = input("Digite o número de telefone: ")
+contato['email'] = input("Digite o endereço de email: ")
+
+print("Informações do contato:", contato)
